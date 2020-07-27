5489ea99081eb36f7e5daf80865f9e6768b145d7
muhsatrio <muhsatrio@gmail.com>
2020-07-27 20:23:27 +0700

    add component for pokemon in home page

--- src/container/PokemonList.js
+++ src/container/PokemonList.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import Pokemon from '../component/Pokemon';
 
 const PokemonList = () => {
     return (
-        <div className="PokemonList">
-            <h1>Home</h1>
+        <div className="PokemonList" style={{display: 'flex', flexWrap: 'wrap'}}>
+            {[1, 2, 3, 4, 5].map(eachData => (
+                <Pokemon key={eachData} pokemon={{id: eachData, img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png', name: 'test'}} />
+            ))}
         </div>
-    )   
+    );
 }
 
 export default PokemonList;
